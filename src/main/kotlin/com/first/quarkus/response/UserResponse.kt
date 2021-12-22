aeeb6fde17e0af7ecc5f70e78e82ed1d974312d3
leoca <leo.cavazzola@hotmail.com>
2021-12-22 19:33:59 -0300

    fix response

--- src/main/kotlin/com/first/quarkus/response/UserResponse.kt
+++ src/main/kotlin/com/first/quarkus/response/UserResponse.kt
@@ -3,12 +3,14 @@ package com.first.quarkus.response
 import com.first.quarkus.model.User
 
 class UserResponse(
+    val id: Long,
     val name: String,
     val email: String,
 )
 
 fun User.toResponse() =
     UserResponse(
-        name = "name",
-        email = "email@email",
+        id = id!!,
+        name = name,
+        email = email,
     )
