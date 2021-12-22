a0e834dac964871a05fefe5e38040233f6d59806
leoca <leo.cavazzola@hotmail.com>
2021-12-22 20:35:09 -0300

    create entity package

--- src/main/kotlin/com/first/quarkus/domain/entity/Bitcoin.kt
+++ src/main/kotlin/com/first/quarkus/domain/entity/Bitcoin.kt
@@ -1,16 +1,18 @@
-package com.first.quarkus.model
+package com.first.quarkus.domain.entity
 
+import java.math.BigDecimal
+import java.time.LocalDate
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-class User(
+class Bitcoin(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
-    val name: String,
-    val email: String,
-    val password: String,
+    val value: BigDecimal,
+    val type: String,
+    val date: LocalDate,
 )
--- src/main/kotlin/com/first/quarkus/domain/entity/User.kt
+++ src/main/kotlin/com/first/quarkus/domain/entity/User.kt
@@ -1,18 +1,16 @@
-package com.first.quarkus.model
+package com.first.quarkus.domain.entity
 
-import java.math.BigDecimal
-import java.time.LocalDate
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
-class Bitcoin(
+class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
-    val value: BigDecimal,
-    val type: String,
-    val date: LocalDate,
+    val name: String,
+    val email: String,
+    val password: String,
 )
