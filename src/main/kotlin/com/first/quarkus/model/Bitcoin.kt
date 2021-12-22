a6a6e5734f09f2a3d53bf7bd63289914be4df628
leoca <leo.cavazzola@hotmail.com>
2021-12-22 00:25:14 -0300

    make Bitcoin an entity

--- src/main/kotlin/com/first/quarkus/model/Bitcoin.kt
+++ src/main/kotlin/com/first/quarkus/model/Bitcoin.kt
@@ -2,9 +2,16 @@ package com.first.quarkus.model
 
 import java.math.BigDecimal
 import java.time.LocalDate
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
+@Entity
 class Bitcoin(
-    val id: Long,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
     val value: BigDecimal,
     val type: String,
     val date: LocalDate,
